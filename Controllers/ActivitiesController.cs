e7bcc634b5502bc5cdce7bb34e18d1b1fe791c7a
Éric Louvard <ericlouvard@Erics-MacBook-Air.local>
2018-11-19 21:23:49 +0100

    Add activities view

---
+++ Controllers/ActivitiesController.cs
@@ -0,0 +1,16 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (http://www.apache.org/licenses/LICENSE-2.0)
+ * See https://github.com/aspnet-contrib/AspNet.Security.OAuth.Providers
+ * for more information concerning the license and the contributors participating to this project.
+ */
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mvc.Client.Controllers
+{
+    public class ActivitiesController : Controller
+    {
+        [HttpGet("~/activities")]
+        public ActionResult Activities() => View();
+    }
+}
