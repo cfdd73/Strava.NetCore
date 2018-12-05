356fb5e3110bcc05d41e350daf00868c4d47dae2
Éric Louvard <ericlouvard@Erics-MacBook-Air.local>
2018-12-05 22:26:38 +0100

    but where is the token?

--- Controllers/ActivitiesController.cs
+++ Controllers/ActivitiesController.cs
@@ -5,19 +5,45 @@
  */
 
 using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Strava.Activities;
+using Strava.Athletes;
+using Strava.Authentication;
+using Strava.Clients;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Mvc.Client.Controllers
 {
+    [Authorize]
     public class ActivitiesController : Controller
     {
-        [HttpGet("~/activities0")]
-        public ActionResult Activities0() => View();
+        public ActivitiesController()
+        {            
+        }
 
         [HttpGet("~/activities"), HttpPost("~/activities")]
         public ActionResult Activities()
         {
             var model=new string[]{"ABCD","EFGH"};
+            Console.WriteLine($"HttpContext.User.Identity.Name={HttpContext.User.Identity.Name}");
+            // Console.WriteLine($"HttpContext.Session={HttpContext.Session.}");
+            // StravaClient client = new StravaClient(User);
             return View(model);
+        }
+        public static async Task GetActivityListAsync(WebAuthentication wa)
+        {
+
+            StravaClient client = new StravaClient(wa);
+            // Receive the currently authenticated athlete
+            List<ActivitySummary> activities = await client.Activities.GetActivitiesAsync(20, 20);
+            // IAuthenticationService authentication  = DependencyResolver.Current.GetService<IAuthenticationService>()
+
+            foreach (ActivitySummary Activity in activities)
+            {
+                Console.WriteLine($"Email:{Activity.Name} {Activity.Type}");
+            }
         }
     }
 }
